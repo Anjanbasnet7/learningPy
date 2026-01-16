74b8cd4c1f41ba35fd25fa2960b0a3a15c662432
Kritan Basnet <nethunter435@gmail.com>
2026-01-16 13:44:08 +0545

    using while loop

--- calculator.py
+++ calculator.py
@@ -1,46 +1,28 @@
-# Calculator using functions
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        return "Cannot divide by zero"
-    return a / b
-
-def calculator():
-    while True:
-        print("\n--- Simple Calculator ---")
-        print("Options: +  -  *  /")
-        
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
-        op = input("Enter operator (+ - * /): ")
-        
-        if op == '+':
-            result = add(a, b)
-        elif op == '-':
-            result = subtract(a, b)
-        elif op == '*':
-            result = multiply(a, b)
-        elif op == '/':
-            result = divide(a, b)
+def calu(firstNum,secondNum,operation):
+    if operation == "+":
+        return firstNum + secondNum
+    elif operation == "-":
+        return firstNum - secondNum
+    elif operation == "*":
+        return firstNum * secondNum
+    elif operation == "/":
+        if secondNum == 0:
+            return "can't divided by zero!"
         else:
-            print("Invalid operator!")
-            continue
-        
-        print(f"Result: {result}")
-        
-        choice = input("Do you want to calculate again? (y/n): ").lower()
-        if choice != 'y':
-            print("Exiting calculator.")
-            break
+            return firstNum / secondNum
+    else:
+        print("Invali input!")
 
-# Run the calculator
-calculator()
+while True:
+    firstNum = float(input("Enter first number:"))
+    secondNum = float(input("Enter second number:"))
+    operation = input("Enter your operation(+ , - , / , *):")
+    result = calu(firstNum,secondNum,operation)
+    print("Result:",result)
+
+    check = input("Do you want to use calcualtor again(y/n)").lower()
+    if check == "n":
+        print("Good bye")
+        break
+    else:
+        print("Please!input the valid(y/n)")
